e57e8fa77394bd376cee905020dffff5f42add5f
rayhan360 <hossainrayhan897@gmail.com>
2023-11-19 21:00:13 +0600

    implement log out and login page sweer alert implement and also fixed a some bug to login page reCaptcha

--- src/pages/Login/Login.jsx
+++ src/pages/Login/Login.jsx
@@ -1,4 +1,4 @@
-import { useContext, useEffect, useRef, useState } from "react";
+import { useContext, useEffect, useState } from "react";
 import {
   loadCaptchaEnginge,
   LoadCanvasTemplate,
@@ -9,9 +9,9 @@ import "./login.css";
 import { AuthContext } from "../../Provider/AuthProvider";
 import { Link } from "react-router-dom";
 import { Helmet } from "react-helmet-async";
+import Swal from "sweetalert2";
 
 const Login = () => {
-  const captchaRef = useRef(null);
   const [disabled, setDisabled] = useState(true);
   const { signIn } = useContext(AuthContext);
 
@@ -28,11 +28,28 @@ const Login = () => {
     signIn(email, password).then((result) => {
       const user = result.user;
       console.log(user);
+      Swal.fire({
+        title: "Login Successfull",
+        showClass: {
+          popup: `
+            animate__animated
+            animate__fadeInUp
+            animate__faster
+          `,
+        },
+        hideClass: {
+          popup: `
+            animate__animated
+            animate__fadeOutDown
+            animate__faster
+          `,
+        },
+      });
     });
   };
 
-  const handleValidateCaptcha = () => {
-    const user_captcha_value = captchaRef.current.value;
+  const handleValidateCaptcha = e => {
+    const user_captcha_value = e.target.value;
 
     if (validateCaptcha(user_captcha_value)) {
       setDisabled(false);
@@ -42,9 +59,9 @@ const Login = () => {
   };
   return (
     <>
-    <Helmet>
-      <title>Bistro Boss || Log In</title>
-    </Helmet>
+      <Helmet>
+        <title>Bistro Boss || Log In</title>
+      </Helmet>
       <div className="hero min-h-screen bg-img">
         <div className="hero-content flex flex-col md:flex-row bg-img shadow-xl p-16">
           <div className="text-center md:w-1/2 lg:text-left">
@@ -82,18 +99,13 @@ const Login = () => {
                 </label>
                 <input
                   type="text"
-                  ref={captchaRef}
+
+                  onBlur={handleValidateCaptcha}
                   placeholder="type the captcha above"
                   name="captcha"
                   className="input input-bordered"
                   required
                 />
-                <button
-                  onClick={handleValidateCaptcha}
-                  className="btn btn-outline btn-xs mt-2"
-                >
-                  Validate
-                </button>
               </div>
               <div className="form-control mt-6">
                 <input
