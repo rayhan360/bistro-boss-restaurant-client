89f6bed8a65f78be2b48f37297b40ae689f6e4d3
rayhan360 <hossainrayhan897@gmail.com>
2023-11-22 11:02:47 +0600

    Delete cart item and sync data using refetch

--- src/pages/Dashboard/Cart/Cart.jsx
+++ src/pages/Dashboard/Cart/Cart.jsx
@@ -1,9 +1,13 @@
 import { BsFillTrash3Fill } from "react-icons/bs";
 import SectionTitle from "../../../components/SectionTitle/SectionTitle";
 import useCart from "../../../hooks/useCart";
+import Swal from "sweetalert2";
+import useAxiosSecure from "../../../hooks/useAxiosSecure";
 
 const Cart = () => {
-  const [cart, isLoading] = useCart();
+  const [cart, isLoading, refetch] = useCart();
+  const axiosSecure = useAxiosSecure();
+
   console.log(cart);
   if (isLoading) {
     return (
@@ -17,6 +21,30 @@ const Cart = () => {
   }
 
   const totalPrice = cart.reduce((total, items) => total + items.price, 0);
+  const handleDelete = (id) => {
+    Swal.fire({
+      title: "Are you sure?",
+      text: "You won't be able to revert this!",
+      icon: "warning",
+      showCancelButton: true,
+      confirmButtonColor: "#3085d6",
+      cancelButtonColor: "#d33",
+      confirmButtonText: "Yes, delete it!",
+    }).then((result) => {
+      if (result.isConfirmed) {
+        axiosSecure.delete(`/carts/${id}`).then((res) => {
+          if (res.data.deletedCount > 0) {
+            Swal.fire({
+              title: "Deleted!",
+              text: "Your file has been deleted.",
+              icon: "success",
+            });
+            refetch()
+          }
+        });
+      }
+    });
+  };
   return (
     <div>
       <SectionTitle
@@ -31,11 +59,15 @@ const Cart = () => {
             Pay
           </button>
         </div>
-        <div className="overflow-x-auto mt-2" style={{borderRadius: "15px 15px 0px 0px"}}>
+        <div
+          className="overflow-x-auto mt-2"
+          style={{ borderRadius: "15px 15px 0px 0px" }}
+        >
           <table className="table">
             {/* head */}
             <thead className="bg-[#D1A054B2] text-white">
               <tr>
+                <th>#</th>
                 <th>Item Image</th>
                 <th>Item Name</th>
                 <th>Price</th>
@@ -43,8 +75,9 @@ const Cart = () => {
               </tr>
             </thead>
             <tbody>
-              {cart.map((item) => (
+              {cart.map((item, index) => (
                 <tr key={item._id}>
+                  <td>{index + 1}</td>
                   <td>
                     <div className="flex items-center gap-3">
                       <div className="avatar">
@@ -63,7 +96,10 @@ const Cart = () => {
                   <td>${item.price}</td>
                   <th>
                     <div className="">
-                      <button className="btn-xs rounded-sm text-base bg-red-600 text-white">
+                      <button
+                        onClick={() => handleDelete(item._id)}
+                        className="btn-xs rounded-sm text-base bg-red-600 text-white"
+                      >
                         <BsFillTrash3Fill />
                       </button>
                     </div>
